46b81375df8b6d1e508af3a2588a7255d493a2da
muhammedjavadksd <javadjazz899@gmail>
2024-07-16 15:49:04 +0530

    Controller & service for upload image

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,28 +1,39 @@
+const FileService = require("../service/FileService");
 const S3BucketClientService = require("../service/S3BucketClient");
 
 
 class UserController{
 
     s3Service;
+    fileService;
 
     constructor() {
-        this.s3Service = new S3BucketClientService("demo")
+        this.s3Service = new S3BucketClientService("demo");
+        this.fileService = new FileService();
         this.uploadImage = this.uploadImage.bind(this);
         this.generatePresignedUrl = this.generatePresignedUrl.bind(this);
     }
 
-    uploadImage(req, res, next) {
+    async uploadImage(req, res, next) {
         const file = req.files.image;
+        const url = req.body.presigned_url;
 
-
-        res.status(200).json({status:"hello"})
+        const uploadImage = await this.fileService.uploadImageToS3Bucket(file, url);
+        if (uploadImage) {
+            res.status(201).json({status:true, msg:"Image upload success"})
+        } else {
+            res.status(400).json({status:false, msg:"Image uploaded failed"})
+        }
     }
-
-
-    generatePresignedUrl(req, res) {
-        const generateURL = this.s3Service.generatedPresignedUrl("abcd");
-        res.status(200).json({url:generateURL})
-        
+    
+    
+    async generatePresignedUrl(req, res) {
+        const generateURL = await this.s3Service.generatedPresignedUrl("abcd");
+        if (generateURL) {
+            res.status(200).json({ url: generateURL, status: false })
+        } else {
+            res.status(400).json({ status: false })
+        }
     }
 }
 
--- src/service/FileService.js
+++ src/service/FileService.js
@@ -2,14 +2,28 @@ const S3BucketClientService = require("./S3BucketClient");
 
 class FileService {
 
-    s3BucketConfig;
-
-    constructor() {
-        this.s3BucketConfig = new S3BucketClientService();
-    }
-
-    uploadImageToS3Bucket(file) {
-        const path="demo"
-        this.s3BucketConfig
+    
+    async uploadImageToS3Bucket(file,presigned_url) {
+        try {
+            
+            const request = await fetch(presigned_url, {
+                method: "PUT",
+                body: file,
+                headers: {
+                    'Content-Type': file.type,
+                }
+            });
+            const response = await request.json();
+            if (!request.ok) {
+                throw new Error("Upload image failed")
+            }
+            console.log("Upload success");
+            return true
+        } catch (e) {
+            console.log("Upload image failed");
+            console.log(e);
+        }
     }
 }
+
+module.exports= FileService
